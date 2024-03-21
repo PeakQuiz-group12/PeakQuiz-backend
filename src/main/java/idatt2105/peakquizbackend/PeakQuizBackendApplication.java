061915cf906f88cb6c07f1738f33710d1498939c
lmlnilse <lmlnilse@stud.ntnu.no>
2024-03-21 20:43:08 +0100

    first commit

---
+++ src/main/java/idatt2105/peakquizbackend/PeakQuizBackendApplication.java
@@ -0,0 +1,13 @@
+package idatt2105.peakquizbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeakQuizBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PeakQuizBackendApplication.class, args);
+  }
+
+}
---
+++ src/test/java/idatt2105/peakquizbackend/PeakQuizBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package idatt2105.peakquizbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeakQuizBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
