13aa066ff1c866a1344c84ac8082da73dd81f7d1
harui <xulr0820@hotmail.com>
2024-03-24 17:53:46 +0100

    Created user entity

---
+++ src/main/java/idatt2105/peakquizbackend/model/User.java
@@ -0,0 +1,23 @@
+package idatt2105.peakquizbackend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="users")
+public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String username;
+
+    private String password;
+}
