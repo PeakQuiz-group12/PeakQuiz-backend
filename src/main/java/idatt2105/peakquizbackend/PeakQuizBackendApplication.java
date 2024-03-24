9f35e02e41adba45453bc17ca3b031b504f5c59b
harui <xulr0820@hotmail.com>
2024-03-24 16:31:49 +0100

    Testing merge rule

--- src/main/java/idatt2105/peakquizbackend/PeakQuizBackendApplication.java
+++ src/main/java/idatt2105/peakquizbackend/PeakQuizBackendApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PeakQuizBackendApplication {
+  //
 
   public static void main(String[] args) {
     SpringApplication.run(PeakQuizBackendApplication.class, args);
