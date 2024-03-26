96b17dcfa994c6db3b841ccec0f9113fd0a27c87
Yasin Ali Marouga <yasinam@stud.ntnu.no>
2024-03-26 19:24:16 +0100

    build: implement new user related entities

---
+++ src/main/java/idatt2105/peakquizbackend/model/MemberType.java
@@ -0,0 +1,8 @@
+package idatt2105.peakquizbackend.model;
+
+public enum MemberType {
+  STUDENT,
+  TEACHER,
+  PROFESSOR,
+  OTHER
+}
---
+++ src/main/java/idatt2105/peakquizbackend/model/User.java
@@ -0,0 +1,66 @@
+package idatt2105.peakquizbackend.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
+
+import java.time.ZonedDateTime;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "USER_TYPE")
+public abstract class User {
+
+  @Id
+  @GenericGenerator(
+          name = "USER_ID_GENERATOR",
+          strategy = "enhanced-sequence",
+          parameters = {
+                 @Parameter(
+                         name = "sequence_name",
+                         value = "USERS_SEQUENCE"
+                 ),
+                 @Parameter(
+                         name = "initial_value",
+                         value = "1000"
+                 )
+          })
+  protected Long id;
+
+  @Temporal(TemporalType.TIMESTAMP)
+  @Column(nullable = false,
+          updatable = false)
+  @CreationTimestamp
+  protected ZonedDateTime createdOn;
+
+  @Size(
+          min = 2,
+          max = 20,
+          message = "Username is required, maximum 20 characters."
+  )
+  @NotNull
+  @Column(nullable = false)
+  protected String username;
+
+  @Size(
+          min = 6,
+          message = "Email is requied, minimum 6 characters."
+  )
+  @NotNull
+  @Column(nullable = false)
+  protected String email;
+
+  // Hash(password + salt)
+  @NotNull
+  @Column(nullable = false)
+  protected String password;
+
+  // Probably not the appropriate datatype
+  @NotNull
+  @Column(nullable = false)
+  protected String salt;
+
+}
