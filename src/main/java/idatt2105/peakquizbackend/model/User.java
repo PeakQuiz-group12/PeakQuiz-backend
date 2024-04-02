5c1d921d74d82b8479bf57d3721f7b1759c729e6
lmlnilse <lmlnilse@stud.ntnu.no>
2024-04-02 11:21:19 +0200

    Added temporary User class

--- src/main/java/idatt2105/peakquizbackend/model/User.java
+++ src/main/java/idatt2105/peakquizbackend/model/User.java
@@ -20,4 +20,9 @@ public class User {
     private String username;
 
     private String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
 }
