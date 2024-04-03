e1dbdc170297bd41308161deb8068b9e5c1cf9f9
harui <xulr0820@hotmail.com>
2024-04-03 21:14:51 +0200

    Remove adding question as quiz becomes null

--- src/main/java/idatt2105/peakquizbackend/PeakQuizBackendApplication.java
+++ src/main/java/idatt2105/peakquizbackend/PeakQuizBackendApplication.java
@@ -6,7 +6,9 @@ import idatt2105.peakquizbackend.model.enums.QuestionType;
 import idatt2105.peakquizbackend.repository.QuestionRepository;
 import idatt2105.peakquizbackend.repository.QuizRepository;
 import jakarta.transaction.Transactional;
-import java.util.HashSet;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -19,29 +21,24 @@ public class PeakQuizBackendApplication {
     SpringApplication.run(PeakQuizBackendApplication.class, args);
   }
 
+  Logger LOGGER = LoggerFactory.getLogger(PeakQuizBackendApplication.class);
   @Bean
   @Transactional
   public CommandLineRunner run (QuizRepository quizRepository, QuestionRepository questionRepository) {
     return (args -> {
       Quiz quiz = new Quiz();
 
-      HashSet<Question> questions = new HashSet<>();
-      Question question = Question.builder()
-          .text("text")
-          .questionType(QuestionType.FILL_IN_BLANK)
-          .difficulty((byte) 1)
-          .build();
-      questions.add(question);
-      quiz.setQuestions(questions);
-
+      LOGGER.info("Saving quiz");
       quizRepository.save(quiz);
 
+      LOGGER.info("Creating questing");
       Question newQuestion = Question.builder()
           .text("text1")
           .questionType(QuestionType.FILL_IN_BLANK)
           .difficulty((byte) 1)
           .quiz(quizRepository.findById(1L).get()).build();
 
+      LOGGER.info("Saved question in quiz");
       questionRepository.save(newQuestion);
 
       System.out.println(quizRepository.findAll());
