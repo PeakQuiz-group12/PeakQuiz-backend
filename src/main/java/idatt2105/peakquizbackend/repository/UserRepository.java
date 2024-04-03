90a80619cc8c6cc26907723ac5505be918c3f1fa
Harry Linrui XU <xulr0820@hotmail.com>
2024-04-03 19:15:18 +0200

    y

--- src/main/java/idatt2105/peakquizbackend/repository/UserRepository.java
+++ src/main/java/idatt2105/peakquizbackend/repository/UserRepository.java
@@ -4,8 +4,6 @@ import idatt2105.peakquizbackend.model.User;
 import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
-
 public interface UserRepository extends CrudRepository<User, Long> {
   Optional<User> findByUsername(String username);
 }
