4924ccfd6f4360bcedba86600ce55e641796c1a5
Harry Linrui XU <xulr0820@hotmail.com>
2024-04-06 17:18:25 +0200

    Create exception for bad input

---
+++ src/main/java/idatt2105/peakquizbackend/exceptions/BadInputException.java
@@ -0,0 +1,8 @@
+package idatt2105.peakquizbackend.exceptions;
+
+public class BadInputException extends RuntimeException {
+
+  public BadInputException(String message) {
+    super(message);
+  }
+}
