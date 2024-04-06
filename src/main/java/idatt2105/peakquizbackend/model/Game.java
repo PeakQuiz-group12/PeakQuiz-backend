b0cf4b294bad5b7b0e0b9600e4b06fc3e64f16c3
Yasin Ali Marouga <yasinam@stud.ntnu.no>
2024-04-07 00:58:17 +0200

    fix: enable persistence of games betwen same user and quiz

--- src/main/java/idatt2105/peakquizbackend/model/Game.java
+++ src/main/java/idatt2105/peakquizbackend/model/Game.java
@@ -4,33 +4,39 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.Immutable;
 
 import java.io.Serializable;
 import java.time.ZonedDateTime;
 
 @Entity
+@Immutable
 @Builder
-@AllArgsConstructor
-@NoArgsConstructor
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @ToString
 public class Game {
   @Embeddable
+  @EqualsAndHashCode
   @NoArgsConstructor
-  @Data
+  @ToString
+  @Setter
   public static class GameId implements Serializable {
+
     @Column(name = "USER_ID")
     private Long userId;
 
     @Column(name = "QUIZ_ID")
     private Long quizId;
 
-    public GameId(Long userId, Long quizId) {
+    @Column(name = "CREATED_ON")
+    private ZonedDateTime createdOn = ZonedDateTime.now();
+
+    public GameId(@NonNull Long userId, @NonNull Long quizId) {
       this.userId = userId;
       this.quizId = quizId;
     }
-    @Override
-    public String toString() { return "{userId: " + userId + "; quizId: " + quizId + "}"; }
   }
 
   @EmbeddedId
@@ -47,24 +53,18 @@ public class Game {
   private String feedback;
 
   @Temporal(TemporalType.TIMESTAMP)
-  @Column(nullable = false,
-          updatable = false)
+  @Column(nullable = false, updatable = false)
   @CreationTimestamp
   private ZonedDateTime playedOn;
 
   // missing like, im unsure about what it signifies
 
   @ManyToOne
-  @JoinColumn(
-          name = "USER_ID",
-          insertable = false, updatable = false)
+  @JoinColumn(name = "USER_ID", insertable = false, updatable = false)
   private User user;
 
   @ManyToOne
-  @JoinColumn(
-          name = "QUIZ_ID",
-          insertable = false, updatable = false
-  )
+  @JoinColumn(name = "QUIZ_ID", insertable = false, updatable = false)
   private Quiz quiz;
 
   public Game(Integer correctAnswers, Byte rating, String feedback, User user, Quiz quiz) {
@@ -73,5 +73,7 @@ public class Game {
     this.feedback = feedback;
     this.user = user;
     this.quiz = quiz;
+    this.id.quizId = quiz.getId();
+    this.id.userId = user.getId();
   }
 }
